e3a768a739194f738c28c1621bf82fec848e8d02
Jake Ryu <jake.ryu@outlook.com>
2023-03-13 21:19:55 +0000

    Implement Main to run the payment service

--- Smartwyre.DeveloperTest.Runner/Program.cs
+++ Smartwyre.DeveloperTest.Runner/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Smartwyre.DeveloperTest.Data;
+using Smartwyre.DeveloperTest.Services;
+using Smartwyre.DeveloperTest.Types;
 
 namespace Smartwyre.DeveloperTest.Runner
 {
@@ -6,7 +9,32 @@ namespace Smartwyre.DeveloperTest.Runner
     {
         static void Main(string[] args)
         {
-            throw new NotImplementedException();
+            var account = new Account
+            {
+                AccountNumber = "1234567", 
+                AllowedPaymentSchemes = AllowedPaymentSchemes.ExpeditedPayments,
+                Balance = 1_000, 
+                Status = AccountStatus.Live
+            };
+            Console.WriteLine($"Current balance of of account ({account.AccountNumber}): {account.Balance}");
+            
+            var service = new PaymentService(new AccountDataStore());
+
+            var request = new MakePaymentRequest { Amount = 200 };
+            var result = service.MakePayment(request);
+            
+            // AccountDataStore is not implemented hence GetAccount() returns null.
+            // Null account is the first condition to check in MakePayment(), so this operation fails.
+            if (result.Success)
+            {
+                Console.WriteLine("Payment made successfully");
+                Console.WriteLine($"New balance: {account.Balance}");
+            }
+            else
+            {
+                Console.WriteLine("Payment was not processed");
+            }
+
         }
     }
 }
